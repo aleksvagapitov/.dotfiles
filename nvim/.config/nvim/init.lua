369ed0589de5739d316fdced92846ad3a0d02f2c
Aleksandr Agapitov <aleksvagapitov@gmail.com>
2023-08-03 07:08:32 +0000

    Updating

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -9,11 +9,10 @@ require "paq" {
     {"nvim-telescope/telescope.nvim", branch="0.1.x"}; 
     "prabirshrestha/asyncomplete.vim"; 
     "mhinz/vim-signify"; 
-    {"neoclide/coc.nvim",·branch="release", run="yarn build"}; 
+    {"neoclide/coc.nvim", branch="release", run="yarn build"}; 
     'junegunn/fzf'; 
     'akinsho/toggleterm.nvim';
     'ojroques/nvim-osc52';
-    --    { 'fatih/vim-go', run = ":GoUpdateBinaries" }; 
 }
 
 require("core")
