1e6da8be1718ef936587bd79bb6421d99b445b0e
Aleksandr Agapitov <aleksvagapitov@gmail.com>
2023-09-13 05:30:20 +0000

    Fixing coc installation

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -9,7 +9,7 @@ require "paq" {
     {"nvim-telescope/telescope.nvim", branch="0.1.x"}; 
     "prabirshrestha/asyncomplete.vim"; 
     "mhinz/vim-signify"; 
-    {"neoclide/coc.nvim", branch="release", run="yarn build"}; 
+    {"neoclide/coc.nvim", branch="release", run="bun build"}; 
     'junegunn/fzf'; 
     'akinsho/toggleterm.nvim';
     'ojroques/nvim-osc52';
