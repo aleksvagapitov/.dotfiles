36c16f5cf42d651e2dce77c0abe5c98e44800958
Aleksandr Agapitov <aleksvagapitov@gmail.com>
2023-09-22 14:01:49 +0000

    Adding go to treesitter

--- nvim/.config/nvim/after/plugin/treesitter.lua
+++ nvim/.config/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "javascript", "typescript", "lua", "c_sharp", "python" },
+  ensure_installed = { "javascript", "typescript", "lua", "c_sharp", "python", "go"},
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
