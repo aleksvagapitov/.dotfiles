404080bf7e9cff9e5e256fe3fb1322febd5a37f9
Aleksandr Agapitov <aleksvagapitov@gmail.com>
2024-09-18 23:41:27 +0000

    Modifying mason config

--- nvim/.config/nvim/after/plugin/mason.lua
+++ nvim/.config/nvim/after/plugin/mason.lua
@@ -1,14 +1,6 @@
 local mason = require("mason")
 local lspconfig = require("lspconfig")
 
--- Check if npm is installed
-local function is_npm_installed()
-    local handle = io.popen("npm --version")
-    local result = handle:read("*a")
-    handle:close()
-    return result and result ~= ""
-end
-
 -- Mason setup
 mason.setup()
 
@@ -18,26 +10,11 @@ require("mason-lspconfig").setup({
         "gopls",     -- Go
         "omnisharp", -- C#
         "pylsp",     -- Python
+        "ts_ls"      -- Typescript
     }
 })
 
-if is_npm_installed() then
-    require("mason-lspconfig").setup({ ensure_installed = { "ts_ls" } })
-end
-
--- LSP configuration for Go, C#, Python, and TypeScript
--- Go
 lspconfig.gopls.setup{}
-
--- C#
 lspconfig.omnisharp.setup{}
-
--- Python
 lspconfig.pylsp.setup{}
-
--- TypeScript (using eslint)
-if is_npm_installed() then
-    lspconfig['ts_ls'].setup{}
-else
-    print("npm is not installed, skipping ts_ls setup")
-end
+lspconfig['ts_ls'].setup{}
