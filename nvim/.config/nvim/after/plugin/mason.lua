8f6806ec5749d582119218537de4d7df99aa836b
Aleksandr Agapitov <aleksvagapitov@gmail.com>
2024-09-18 21:52:37 +0000

    Attempting to fix ts_ls installation

--- nvim/.config/nvim/after/plugin/mason.lua
+++ nvim/.config/nvim/after/plugin/mason.lua
@@ -1,17 +1,31 @@
-require("mason").setup()
+local mason = require("mason")
+local lspconfig = require("lspconfig")
 
+-- Check if npm is installed
+local function is_npm_installed()
+    local handle = io.popen("npm --version")
+    local result = handle:read("*a")
+    handle:close()
+    return result and result ~= ""
+end
+
+-- Mason setup
+mason.setup()
+
+-- Ensure LSP servers are installed
 require("mason-lspconfig").setup({
     ensure_installed = {
         "gopls",     -- Go
         "omnisharp", -- C#
         "pylsp",     -- Python
-        "eslint"     -- TypeScript 
     }
 })
 
--- LSP configuration for Go, C#, Python, and TypeScript
-local lspconfig = require("lspconfig")
+if is_npm_installed() then
+    require("mason-lspconfig").setup({ ensure_installed = { "ts_ls" } })
+end
 
+-- LSP configuration for Go, C#, Python, and TypeScript
 -- Go
 lspconfig.gopls.setup{}
 
@@ -21,10 +35,9 @@ lspconfig.omnisharp.setup{}
 -- Python
 lspconfig.pylsp.setup{}
 
--- TypeScript (using ts_ls)
-lspconfig.eslint.setup{
-    cmd = { "bun", "eslint", "--stdio" },
-    settings = {
-        workingDirectory = { mode = "auto" }
-    }
-}
+-- TypeScript (using eslint)
+if is_npm_installed() then
+    lspconfig['ts_ls'].setup{}
+else
+    print("npm is not installed, skipping ts_ls setup")
+end
